65b93f3e8c9a91c899f1178f4715a534e195710d
Daniel Araújo <daniels_araujo@outlook.com.br>
2025-08-28 20:46:17 -0300

    05.06 - Classes de Configuração

--- DemoVS/DemoVS/Program.cs
+++ DemoVS/DemoVS/Program.cs
@@ -1,6 +1,18 @@
+// Configuração do Builder
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Configuração do Pipeline
+// Middleware
+// Services
+
+// Configuração da App
+
 var app = builder.Build();
 
+// Configuração de Comportamentos da App
+
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/teste", () => "Isso é um teste!");
 
 app.Run();
