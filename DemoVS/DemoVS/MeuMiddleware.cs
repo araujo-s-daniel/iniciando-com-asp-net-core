3cbec95a8618824caa5301f42da4ab1e5a55216b
Daniel Araújo <daniels_araujo@outlook.com.br>
2025-08-30 08:08:24 -0300

    06.03 - Middlewares na prática

---
+++ DemoVS/DemoVS/MeuMiddleware.cs
@@ -0,0 +1,43 @@
+﻿using Serilog;
+using System.Diagnostics;
+
+namespace DemoVS
+{
+    public class TemplateMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public TemplateMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            // Lógica antes de chamar o próximo middleware
+            await _next(context);
+            // Lógica após chamar o próximo middleware
+        }
+    }
+
+    public class LogTempoMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public LogTempoMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            // Lógica antes de chamar o próximo middleware
+            var sw = Stopwatch.StartNew();
+
+            await _next(context);
+
+            // Lógica após chamar o próximo middleware
+            sw.Stop();
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+            Log.Information($"A execução demorou: {sw.ElapsedMilliseconds} ms ({sw.Elapsed.TotalSeconds}) segundos");
+        }
+    }
+}
--- DemoVS/DemoVS/Program.cs
+++ DemoVS/DemoVS/Program.cs
@@ -1,8 +1,11 @@
 // Configuração do Builder
 
+using DemoVS;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Configuração do Pipeline
+
 // Middleware
 // Services
 
@@ -12,7 +15,13 @@ var app = builder.Build();
 
 // Configuração de Comportamentos da App
 
+app.UseMiddleware<LogTempoMiddleware>();
+
 app.MapGet("/", () => "Hello World!");
-app.MapGet("/teste", () => "Isso é um teste!");
+app.MapGet("/teste", () =>
+{
+    Thread.Sleep(1500);
+    return "Teste 2";
+});
 
 app.Run();
